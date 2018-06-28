e590fac103e7a855a76a67235388f21f48181812
Johannes Eiglsperger <git@remodding.de>
2018-06-28 22:21:36 +0200

    Add subdomain to cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,6 +3,6 @@
 set -e
 
 PROXY_URL="https://127.0.0.1:8000/v1"
-ACME_DOMAIN="${CERTBOT_DOMAIN}"
+ACME_DOMAIN="_acme-challenge.${CERTBOT_DOMAIN}"
 
 curl -s -X DELETE -n "${PROXY_URL}/cleanup?fqdn=${ACME_DOMAIN}"
